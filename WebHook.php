97748cce30cf82a873af0a1a40e20110bb11d5fd
RFContracts <45063715+RFContracts@users.noreply.github.com>
2019-10-18 16:10:43 +0300

    Update WebHook.php

--- WebHook.php
+++ WebHook.php
@@ -1,19 +1,26 @@
 <?php
-/**
- * Web hook is receiver data from outside, processing and sending it to BitrixCRM
- */
+include('Base.php');
 
+/**
+ * Web hook is receiver data from outside, processing and sending it to Bitrix24
+ */
 class WebHook extends Base
 {
 
     /**
-     * Api url for BitrixCRM
+     * Api url for Bitrix24
      * @var string
      */
-    public $apiUrl = 'https://a-nevski.bitrix24.ru/rest/1/1o88wyq5m0yizz68/';
+    public $apiUrl = 'https://a-nevski.bitrix24.ru/rest/1/1o88wyq5m0yizz68';
 
     /**
-     * Business process id for BitrixCRM
+     * Api url for Triggers
+     * @var
+     */
+    protected $triggerApiUrl = 'https://a-nevski.bitrix24.ru/rest/1/yjonnh47ijv34jjh';
+
+    /**
+     * Business process id for Bitrix24
      * @var int
      */
     public $businessProcessId = 346;
